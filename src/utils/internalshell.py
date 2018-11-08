a0a2231e6bcb58cf07f92e88535f1deff1f40a68
Michele Pirola <mic.pirola@gmail.com>
2018-11-08 16:42:13 +0000

    possibile bonus/malus nascosto

--- src/utils/internalshell.py
+++ src/utils/internalshell.py
@@ -8,26 +8,34 @@ app = App.get_running_app()
 class internalShell(cmd.Cmd):
 
     def do_bonus(self, line):
-        'bonus [last_name] [amount] - gives bonus to selected team'
+        'bonus [last_name] [amount] ["hidden"] - gives bonus to selected team'
         try:
             bonus_team_lastName = line.split(' ')[0]
             bonus_team_id = app.dictIDLastName.keys()[app.dictIDLastName.values().index(unicode(bonus_team_lastName))]
             bonus_amount = int(line.split(' ')[1])
-
-            if bonus_amount > 0:
-                popup_title = '+' + str(bonus_amount) + ' punti'
-                popup_color = [0,204./255.,0,1]
-            else:
-                popup_title = str(bonus_amount) + ' punti'
-                popup_color = [1,0,0,1]
-
-            popup_content = "[size=40]SQUADRA[/size]\n\n[b]" + str(app.dictIDName[bonus_team_id]) + "[/b]"
+            try:
+                bonus_hidden = (line.split(' ')[2] == 'hidden')
+            except:
+                bonus_hidden = False
 
             if bonus_amount != 0:
                 app.GENERAL_SCORE[bonus_team_id] += bonus_amount
+
                 print "bonus to " + str(app.dictIDName[bonus_team_id]) + ": " + str(bonus_amount)
-                popup = Popup(title=popup_title, title_align='center', title_color=popup_color, title_size='50sp', title_font='font/UbuntuMono-B.ttf', separator_color=popup_color, content=Label(text=popup_content, font_size=80, font_name='font/UbuntuMono-B.ttf', halign='center', markup=True),size_hint=(None, None), size=(800, 600))
-                popup.open()
+
+                if not bonus_hidden:
+
+                    if bonus_amount > 0:
+                        popup_title = '+' + str(bonus_amount) + ' punti'
+                        popup_color = [0,204./255.,0,1]
+                    else:
+                        popup_title = str(bonus_amount) + ' punti'
+                        popup_color = [1,0,0,1]
+
+                    popup_content = "[size=40]SQUADRA[/size]\n\n[b]" + str(app.dictIDName[bonus_team_id]) + "[/b]"
+                    popup = Popup(title=popup_title, title_align='center', title_color=popup_color, title_size='50sp', title_font='font/UbuntuMono-B.ttf', separator_color=popup_color, content=Label(text=popup_content, font_size=80, font_name='font/UbuntuMono-B.ttf', halign='center', markup=True),size_hint=(None, None), size=(800, 600))
+                    popup.open()
+
                 if app.SECTIONS[app.SEC_CNT]['type'] == 'test':
                     print "WARNING: score will reset after test section!"
             else:
