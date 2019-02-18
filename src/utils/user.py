cf355250434326d1f57c42d3490e9df2ef7dc2b0
Michele Pirola <mic.pirola@gmail.com>
2019-02-18 13:04:14 +0000

    img ricavata da cognome, codici non più duplicati

--- src/utils/user.py
+++ src/utils/user.py
@@ -47,6 +47,7 @@ def setGlobal(init_file):
     app.dictIDBonus = {}
     app.dictIDReset = {}
     app.dictIDLastName = {}
+    app.dictIDicona = {}
     for id in config['Squadre']:
         app.dictIDBonus[id] = int(config.get('Squadre',id).split(',')[0])
         app.dictIDReset[id] = int(config.get('Squadre',id).split(',')[0])
@@ -55,6 +56,7 @@ def setGlobal(init_file):
             app.dictIDLastName[id] = (config.get('Squadre',id).split(',')[1].split(' ')[1]+'_'+config.get('Squadre',id).split(',')[1].split(' ')[2]).lower().encode('utf-8')
         except:
             app.dictIDLastName[id] = config.get('Squadre',id).split(',')[1].split(' ')[1].lower().encode('utf-8')
+        app.dictIDicona[id] = app.dictIDLastName[id].decode('utf-8')+".png"
 
     app.PositionBefore = dict(zip(app.dictIDName.keys(), [0]*len(app.dictIDName.keys())))
     app.Position = dict(zip(app.dictIDName.keys(), [0]*len(app.dictIDName.keys())))
@@ -100,34 +102,3 @@ def setGlobal(init_file):
 
     app.starting_counter = 0
     app.starting_classifica = dict(zip(app.dictIDName.keys(), [0]*len(app.dictIDName.keys())))
-
-    app.dictIDicona = { "977148330": "einstein.png",
-                        "977144982": "newton.png",
-                        "977143789": "planck.png",
-                        "977143982": "feynman.png",
-                        "977141870": "fermi.png",
-                        "977148973": "maxwell.png",
-                        "977186140": "schrodinger.png",
-                        "977149543": "galileo.png",
-                        "981430530": "volta.png",
-                        "977151320": "curie.png",
-                        "977149772": "heisenberg.png",
-                        "977185902": "dirac.png",
-                        "981465612": "tesla.png",
-                        "981466912": "bohr.png",
-                        "977184646": "joule.png",
-                        "977186205": "pauli.png",
-                        "977143950": "faraday.png",
-                        "977185877": "rutherford.png",
-                        "977148317": "hawking.png",
-                        "977186733": "noether.png",
-                        "981413202": "boltzmann.png",
-                        "981464356": "keplero.png",
-                        "977186685": "copernico.png",
-                        "977143946": "ampere.png",
-                        "977183543": "born.png",
-                        "977149774": "majorana.png",
-                        "981466864": "von_neumann.png",
-                        "977186485": "becquerel.png",
-                        "977183883": "bernoulli.png",
-                        "977149683": "pascal.png" }
